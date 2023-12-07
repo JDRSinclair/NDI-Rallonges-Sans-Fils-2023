15411756bf1b593b030fc75ca7fb377d8e535975
Leho Errezarret <errezarretleho@gmail.com>
2023-12-08 00:11:37 +0100

    Premiere structure du projet, Menu + Game1 + Game2

--- projetNDI-LRSF/src/routes/+page.svelte
+++ projetNDI-LRSF/src/routes/+page.svelte
@@ -1,2 +1,51 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Settings from "./Settings.svelte";
+
+    let settingsOpen = false;
+    let fieldTest;
+
+    function handleClickSettings()
+    {
+        console.log(settingsOpen);
+        if(settingsOpen)
+        {
+            settingsOpen = false;
+        }
+        else
+        {
+            settingsOpen = true;
+        }
+    }
+
+</script>
+
+<body>
+    <div id="mainDiv">
+        <h1>Menu du jeux</h1>
+        <a href="/game1">Jeu 1</a>
+        <a href="/game2">Jeu 2</a>
+        <a href="#" on:click={handleClickSettings}>Options</a>
+        
+        {#if settingsOpen}
+            <Settings/>
+        {/if}  
+    </div>
+</body>
+
+<style>
+    html, body {
+    height: 100%;
+    margin: 0;
+    overflow: hidden;
+    background-color: #ccc; /* Ajoute une couleur de fond pour mieux voir la div */
+}
+
+
+#mainDiv {
+    width: 100vw;
+    height: 56.25vw; /* 16/9 aspect ratio */
+    max-height: 100vh;
+    background-color: white; /* Ajoute une couleur de fond pour mieux voir la div */
+    
+}
+</style>
---
+++ projetNDI-LRSF/src/routes/Settings.svelte
@@ -0,0 +1,24 @@
+<body>
+    <div id="mainDiv">
+            
+    </div>
+    </body>
+    
+    <style>
+        html, body {
+        height: 100%;
+        margin: 0;
+        overflow: hidden;
+        background-color: #ccc; /* Ajoute une couleur de fond pour mieux voir la div */
+    }
+    
+    
+    #mainDiv {
+        width: 100vw;
+        height: 56.25vw; /* 16/9 aspect ratio */
+        max-height: 100vh;
+        background-size: cover; /* Ajuste la taille du fond pour couvrir toute la div */
+        background-position: center; /* Centre le fond */
+        background-repeat: no-repeat; /* Empêche la répétition du fond */
+        background-image: url("./assets/fond.png");}
+    </style>
