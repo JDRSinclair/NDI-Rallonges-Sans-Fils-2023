90c0e356a42c86b9a12c0fe4428b44b5b2b882c2
Leho Errezarret <errezarretleho@gmail.com>
2023-12-08 05:48:16 +0100

    Auto stash before merge of "main" and "origin/main"

--- projetNDI-LRSF/src/routes/game1/+page.svelte
+++ projetNDI-LRSF/src/routes/game1/+page.svelte
@@ -5,6 +5,12 @@
     let currentBuildingIndex = 0;
     let currentBuilding = { ...buildings[currentBuildingIndex] };
     let showPopup = false;
+
+    var bat1 = -1;
+    var bat2 = -1;
+    var bat3 = -1;
+    var bat4 = -1;
+    var bat5 = -1;
   
     function getNextBuilding() {
       currentBuildingIndex = (currentBuildingIndex + 1) % buildings.length;
@@ -27,22 +33,26 @@
         {/if}
         <!-- svelte-ignore missing-declaration -->
         <div on:click={() => _handleClickButton(1)} class="positionDiv" id="place1" style="top:50%; left:5%">
-            <img style="width:100%; height:100%" src='src\lib\images\geothermique.png'  alt="Description de l'image"/>
+            {#if bat1 != -1}<img style="width:100%; height:100%" src={bat1}  alt="Description de l'image"/>{/if}
         </div>
         <div on:click={() => _handleClickButton(2)} class="positionDiv" id="place2" style="top:45%; left:25%">
-
+            {#if bat2 != -1}<img style="width:100%; height:100%" src={bat2}  alt="Description de l'image"/>{/if}
         </div>
         <div on:click={() => _handleClickButton(3)} class="positionDiv" id="place3" style="top:70%; left:40%">
-
+            {#if bat3 != -1}<img style="width:100%; height:100%" src={bat3}  alt="Description de l'image"/>{/if}
         </div>
         <div on:click={() => _handleClickButton(4)} class="positionDiv" id="place4" style="top:40%; left:60%; width:17vh; height:17vh">
-
+            {#if bat4 != -1}<img style="width:100%; height:100%" src={bat4}  alt="Description de l'image"/>{/if}
         </div>
         <!-- svelte-ignore missing-declaration -->
         <div on:click={() => _handleClickButton(5)} class="positionDiv" id="place5" style="top:45%; left:85%">
-
+            {#if bat5 != -1}<img style="width:100%; height:100%" src={bat5}  alt="Description de l'image"/>{/if}
         </div>
-        <!---->
+        
+        <div class="ui" style="top:85%; left:0%">
+            <p>Argent :</p> <progress value="50" max="100"></progress>
+            <p>Polution : </p> <progress value="50" max="100"></progress>           
+        </div>
     </div>
 </body>
 
@@ -72,5 +82,16 @@
         width: 20vh;
         position: absolute;
         cursor: pointer;
+    }
+
+    .ui
+    {
+        background-color: rgba(200, 100, 20, 100%);
+        border-radius: 5px;;
+        height: 15vh;
+        width: 40vh;
+        position: absolute;
+        cursor: pointer;
+        padding: 10px;
     }
 </style>
